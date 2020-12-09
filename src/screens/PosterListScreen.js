773b388b73b4d43139f93a7a4a7c7a52c50b3beb
junamariajohn <junamaria98@gmail.com>
2020-12-09 14:33:03 +0530

    newpull

--- src/screens/PosterListScreen.js
+++ src/screens/PosterListScreen.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import {
   View,
   FlatList,
@@ -8,9 +8,9 @@ import {
   ScrollView,
   TouchableOpacity,
 } from 'react-native';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 import PosterGridItem from '../components/PosterGridItem';
-import {AuthContext} from '../context/authContext';
+import { AuthContext } from '../context/authContext';
 
 const PosterListScreen = (props) => {
   const auth = useContext(AuthContext);
@@ -34,12 +34,12 @@ const PosterListScreen = (props) => {
   };
 
   return (
-    <View style={{flex: 1, backgroundColor: '#fafafa'}}>
+    <View style={{ flex: 1, backgroundColor: '#fafafa' }}>
       <FlatList
-        style={{backgroundColor: '#white'}}
+        style={{ backgroundColor: '#white' }}
         keyExtractor={(item) => item.id}
         data={posters}
-        renderItem={({item}) => (
+        renderItem={({ item }) => (
           <PosterGridItem
             id={item._id}
             poster={item.title}
